f865ef00ea0bab6fd42e0a841ed383edeee4faf8
Everson Soucek <sousekjuniorr@outlook.com>
2023-01-26 14:28:54 -0300

    ultimo commit

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,16 +1,25 @@
 import Banner from 'components/Banner'
 import Card from 'components/Card'
 import Titulo from 'components/Titulo'
-import React from 'react'
-import videos from 'json/db.json'
+import React, { useEffect, useState } from 'react'
 import styles from './Inicio.module.css'
 
 export default function Inicio() {
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/EversonSoucek/CineTag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
     return (
         <>
             <Banner imagem='home' />
             <Titulo>Um lugar para guardar seus vídeos e filmes! </Titulo>
-            <section className={styles.container}>{videos.map((video) => (<Card {...video} key={video.id}></Card>))}</section>
+            <section className={styles.container}>{videos.map((video) => { return <Card {...video} key={video.id}></Card> })}</section>
         </>
 
 
